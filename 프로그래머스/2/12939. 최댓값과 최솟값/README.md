3797187a3a2a322b351f6b028e42317d6b8052f6
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2023-11-10 10:40:19 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.17 ms
+메모리: 33.4 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 16:11:33
+2023년 11월 5일 10:40:17
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,17 +1,4 @@
 function solution(s) {
-    // 1. 저장할 최대값, 최소값
-    let max = -Number.MAX_VALUE;
-    let min = Number.MAX_VALUE;
-    
-    // 2. string -> array(정수로 변환)
-    let arr = s.split(' ').map((n)=>parseInt(n))
-
-    // 3. 배열 돌면서 비교
-    for (let i=0; i<arr.length; i++) {
-        max = Math.max(max, arr[i])
-        min = Math.min(min, arr[i])
-    }
-    
-    //4. 출력
-    return min.toString() + ' ' + max.toString();
+    const arr = s.split(' ').map(v=>+v).sort((a, b) => a - b);
+    return arr[0] + ' ' + arr[arr.length - 1];
 }
