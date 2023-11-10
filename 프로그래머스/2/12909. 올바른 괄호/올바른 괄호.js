d88dc4ff8dcecdecfc1aa8ceda996f90f01b2842
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2023-11-10 10:46:31 +0900

    [level 2] Title: 올바른 괄호, Time: 2.44 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,12 @@
+function solution(s){
+    var answer = true;
+    let st = 0;
+    for (let i=0; i<s.length; i++) {
+        if (s[i] === '(') st += 1
+        else {
+            if (st > 0) st -= 1
+            else return false
+        }
+    }
+    return st === 0 ? true : false;
+}
