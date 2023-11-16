60a0e8009cfd55b10e09350bc2932401baadb032
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2023-11-16 17:22:46 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,16 +1,7 @@
-const count = (n) => {
-    let cnt = 0;
-    for (let i=1; i<Math.sqrt(n); i++) {
-        if(n % i === 0) cnt++;
-    }
-    return Math.sqrt(n) === parseInt(Math.sqrt(n)) ? 2*cnt+1 : 2*cnt;
-}
-
 function solution(left, right) {
     let answer = 0;
     for (let i=left; i<=right; i++) {
-        const cnt = count(i)
-        cnt % 2 === 0 ? answer += i : answer -= i
+        Math.sqrt(i) !== parseInt(Math.sqrt(i)) ? answer += i : answer -= i
     }
     return answer;
 }
