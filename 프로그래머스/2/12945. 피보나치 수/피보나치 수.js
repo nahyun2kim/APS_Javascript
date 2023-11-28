4962fa18fd34d81e1012ca73d0dcce5f8469f8f9
나 현 <107095081+nahyun2kim@users.noreply.github.com>
2023-11-28 13:48:58 +0900

    [level 2] Title: 피보나치 수, Time: 5.80 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,7 @@
+function solution(n) {
+    const fibonacci = [0, 1]
+    for(let i=2; i<=n; i++) {
+        fibonacci.push((fibonacci[i-2] + fibonacci[i-1]) % 1234567)
+    } 
+    return fibonacci[n];
+}
